74aa2cf181f9e696f97f6db3b9070351b254ac1d
Scripthqs <1941186772@qq.com>
2021-09-05 15:31:56 +0800

    2021-09-05

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 now=$(date "+%Y-%m-%d")
-echo "更改路径"
+echo "进入项目目录"
 cd D:\user\Desktop\scripthqs\assets
 echo "开始提交"
 git add -A && git commit -m "$now" && git pull && git push
-echo "完成"
+echo "提交完成"
 read
